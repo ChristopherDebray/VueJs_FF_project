6b1a6c0f4055e7cdbfc72fbf2108ef166f89f432
Christopher Debray <christopher.debray@club-employes.com>
2022-12-14 17:06:13 +0100

    ADD files / folders for css and configs for router and tailwind

---
+++ front_project/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <nav class="bg-secondary">
+        <div>
+            <router-link to="/">Home</router-link>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+</style>
--- front_project/tailwind.config.cjs
+++ front_project/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    textColor: {
+      'primary': '#404040',
+    },
+    backgroundColor: {
+      'primary': '#e6e6e6',
+      'secondary': 'white',
+    },
   },
   plugins: [],
 }

56ca78554088c081df0c32e7e67ab5b7d2b13ba8
Christopher Debray <christopher.debray@club-employes.com>
2023-01-13 11:21:49 +0100

    ADD characterPreviewCard component and tests

--- front_project/src/components/cards/CharacterPreviewCard.spec.js
+++ front_project/src/components/cards/CharacterPreviewCard.spec.js
@@ -6,28 +6,26 @@ import { mount } from '@vue/test-utils';
 import CharacterPreview from './src/components/cards/CharacterPreviewCard.vue';
 
 describe('check CharacterPreviewCard', () => {
-    const propsTestData = {
-        "Avatar": "https:\/\/img2.finalfantasyxiv.com\/f\/5b99cd8dd0208c7aba0e8f5c66f83f94_c33f640c0cdd35f7def85b8aa31a0007fc0_96x96.jpg?1673535638",
-        "ID": 45663480,
-        "Lang": "FR",
-        "Name": "Pinkuy Pingu",
-        "Server": "Twintania"
-    };
-
     it('should be render with it\'s props values', () => {
-        const wrapper = mount(CharacterPreview, {
-            propsData: {
-                "Avatar": "https:\/\/img2.finalfantasyxiv.com\/f\/5b99cd8dd0208c7aba0e8f5c66f83f94_c33f640c0cdd35f7def85b8aa31a0007fc0_96x96.jpg?1673535638",
-                "ID": 45663480,
-                "Lang": "FR",
-                "Name": "Pinkuy Pingu",
-                "Server": "Twintania"
+        const propsTestData = {
+            character: {
+                Avatar: "https:\/\/img2.finalfantasyxiv.com\/f\/5b99cd8dd0208c7aba0e8f5c66f83f94_c33f640c0cdd35f7def85b8aa31a0007fc0_96x96.jpg?1673535638",
+                ID: 45663480,
+                Lang: "FR",
+                Name: "Pinkuy Pingu",
+                Server: "Twintania"
             }
+        };
+        const testCharacter = propsTestData.character;
+
+        const wrapper = mount(CharacterPreview, {
+            propsData: propsTestData
         });
-        expect(wrapper.find('.characterPreview__avatar').element.getAttribute('src')).toBe(propsTestData.Avatar)
-        expect(wrapper.find('.characterPreview__avatar').element.getAttribute('alt')).toBe(`Image of ${propsTestData.Name}`)
-        expect(wrapper.find('.characterPreview__lang').element.textContent).toBe(propsTestData.Lang)
-        expect(wrapper.find('.characterPreview__name').element.textContent).toBe(propsTestData.Name)
-        expect(wrapper.find('.characterPreview__server').element.textContent).toBe(propsTestData.Server)
+
+        expect(wrapper.find('.characterPreview__avatar').element.getAttribute('src')).toBe(testCharacter.Avatar)
+        expect(wrapper.find('.characterPreview__avatar').element.getAttribute('alt')).toBe(`Image of ${testCharacter.Name}`)
+        expect(wrapper.find('.characterPreview__lang').element.textContent).toBe(testCharacter.Lang)
+        expect(wrapper.find('.characterPreview__name').element.textContent).toBe(testCharacter.Name)
+        expect(wrapper.find('.characterPreview__server').element.textContent).toBe(testCharacter.Server)
     })
 })
--- front_project/src/components/cards/CharacterPreviewCard.vue
+++ front_project/src/components/cards/CharacterPreviewCard.vue
@@ -1,8 +1,22 @@
 <script setup>
+defineProps({
+    character: Object
+    /*
+    avatar: String,
+    name: String,
+    server: String,
+    lang: String
+    */
+})
 </script>
 
 <template>
-    
+    <div class="characterPreviewCard">
+        <img :src="character.Avatar" :alt="'Image of '+character.Name" class="characterPreview__avatar" />
+        <p class="characterPreview__name">{{ character.Name }}</p>
+        <p class="characterPreview__server">{{ character.Server }}</p>
+        <p class="characterPreview__lang">{{ character.Lang }}</p>
+    </div>
 </template>
 
 <style scoped>
